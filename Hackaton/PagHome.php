cf3905803ed8c1c37452f185c6fceaa41c669f35
ratasi <rafatarre@gmail.com>
2016-09-13 16:38:57 +0200

    Add files via upload

---
+++ Hackaton/PagHome.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+
+<header>
+	<img id="logo" src="img/logo.png"></img>
+	<img class="cat" src="img/actua.png"></img>
+	<img class="cat" src="img/sucesos.png"></img>
+	<img class="cat" src="img/trucos.png"></img>
+	<img class="Pokedex" src="img/Pokedex1.png" onmouseover="this.src='img/Pokedex2.png';" onmouseout="this.src='img/Pokedex1.png';"/></img>
+	
+	<div id="linea"></div>	
+</header>	
+
+
+
+
+</body>
+</html>
