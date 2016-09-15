4453e0655bb552cc87837c8d888a3b3bac5058e7
Adria13 <adrichafes97@gmail.com>
2016-09-15 12:50:11 +0200

    Add files via upload

---
+++ HGN/views/PagHome2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<header>
+		<img id="logo" src="img/logo.png"></img>
+		<img class="cat" src="img/actua.png"></img>
+		<img class="cat" src="img/sucesos.png"></img>
+		<img class="cat" src="img/trucos.png"></img>
+		<img class="Pokedex" src="img/Pokedex1.png" onmouseover="this.src='img/Pokedex3.png';" onmouseout="this.src='img/Pokedex1.png';"/></img>
+		<div id="linea"></div>	
+	</header>	
+
+
+	<main id="main">
+		<section>
+			<h1> PRUEBA MAIN </h1>
+		</section>
+	</main>
+
+	<footer>
+		<p>Hackathon GO NEWS | <a href="http://goo.gl/x4tE2M">Florida Universitaria</a></p>
+
+	</footer>
+</body>
+
+</html>
