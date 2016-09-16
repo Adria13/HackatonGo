b4051aa4014a3bf2f3525f9a06770e0fe1ece361
SrGio <sergionavarronavarro@msn.com>
2016-09-16 13:06:20 +0200

    Add files via upload

---
+++ HGN/views/page.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+	#CSS#
+
+</head>
+<body>
+
+<header>
+	<img id="logo" src="img/logo.png"></img>
+	<a href="index.php?action=principal&Id_categoria=1"><img class="cat" src="img/actua.png"></img></a>
+	<a href="index.php?action=principal&Id_categoria=2"><img class="cat" src="img/sucesos.png"></img></a>
+	<a href="index.php?action=principal&Id_categoria=3"><img class="cat" src="img/trucos.png"></img></a>
+	<div id="pokedex"><a href="#"><img id="dex" src="img/pokedex1.png" onclick="this.src='img/Pokedex2.png';" onmouseout="this.src='img/Pokedex1.png';"/></img><span>Iniciar</span></a></div>
+
+	<div id="linea1"></div>	
+</header>	
+<content>
+#SLIDER#
+	<div id="part2">
+
+	#CONTENIDO#
+
+
+
+	<footer>
+		<p>Hackathon GO NEWS | <a href="http://goo.gl/x4tE2M">Pokemon GO official web</a></p>
+
+	</footer>
+
+
+</body>
+</html>
