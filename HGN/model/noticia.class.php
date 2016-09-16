0044d80253ceb5ef12ff3cf8b506c545e6458cc8
SrGio <sergionavarronavarro@msn.com>
2016-09-16 13:05:20 +0200

    Add files via upload

--- HGN/model/noticia.class.php
+++ HGN/model/noticia.class.php
@@ -25,9 +25,10 @@
 		function resumen(){
 
 		}
-		function listar($cat){
+
+		function listartodas(){
 			if($c=$this->conectar()){
-				$sentencia="SELECT * FROM noticia n RIGHT JOIN categoria c WHERE n.Id_noticia=c.Id_noticia AND c.Id_categoria=".$cat;
+				$sentencia="SELECT * FROM noticia";
 				if($this->consulta($sentencia)){
 					$resultado=$this->consulta($sentencia);
 					//var_dump($resultado);
@@ -45,8 +46,77 @@
 				echo "Error al conectar con la base de datos";
 			}
 		}
-		function valoracion(){
 
+		function listar($cat){
+			if($c=$this->conectar()){
+				$sentencia="SELECT * FROM noticia n JOIN not_cat c WHERE n.Id_noticia=c.Id_noticia AND c.Id_categoria=".$cat;
+				if($this->consulta($sentencia)){
+					$resultado=$this->consulta($sentencia);
+					//var_dump($resultado);
+					while($objeto=mysqli_fetch_object($resultado)){
+						//var_dump($objeto);
+						$array[]=$objeto;
+					}
+					//var_dump($array);
+					$this->desconectar();
+					return $array;
+				}else{
+	 				echo "Error con la sentencia";
+				}
+			}else{
+				echo "Error al conectar con la base de datos";
+			}
+		}
+		function relacionadas($not){
+			if($c=$this->conectar()){
+				$sentencia="SELECT * FROM noticia WHERE Id_noticia=(
+SELECT Id_noticia FROM not_cat WHERE Id_categoria=(
+SELECT c.Id_categoria FROM noticia n JOIN not_cat c WHERE n.Id_noticia=c.Id_noticia AND n.Id_noticia=".$not.")) LIMIT 2";
+				if($this->consulta($sentencia)){
+					$resultado=$this->consulta($sentencia);
+					//var_dump($resultado);
+					while($objeto=mysqli_fetch_object($resultado)){
+						//var_dump($objeto);
+						$array[]=$objeto;
+					}
+					//var_dump($array);
+					$this->desconectar();
+					return $array;
+				}else{
+	 				echo "Error con la sentencia";
+				}
+			}else{
+				echo "Error al conectar con la base de datos";
+			}
+		}
+		function valoracion($not){
+			if($c=$this->conectar()){
+				$sentencia="SELECT valor FROM valoracion WHERE Id_noticia=".$not;
+				if($this->consulta($sentencia)){
+					$resultado=$this->consulta($sentencia);
+					//var_dump($resultado);
+					while($objeto=mysqli_fetch_object($resultado)){
+						//var_dump($objeto);
+						$array[]=$objeto;
+					}
+					//var_dump($array);
+					$this->desconectar();
+				}else{
+	 				echo "Error con la sentencia";
+				}
+				//var_dump($array);
+				$valor=0;
+				if(sizeof($array)!=0){
+					for($i=0;$i<sizeof($array);$i++){
+						//var_dump($valor);
+						//var_dump($array[$i]->valor);
+						$valor=$valor+$array[$i]->valor;
+					}
+					//var_dump($valor);
+					$valor=$valor/sizeof($array);
+				}
+				return $valor;
+			}
 		}
 	}
 
--- HGN/model/usuario.class.php
+++ HGN/model/usuario.class.php
@@ -2,13 +2,65 @@
 	require_once("db.class.php");
 	class Usuario extends Database{
 		function registrar(){
+			if($this->conectar()){
+				$nombre=$_POST['nombre_usuario'];
+				$contrasenya=md5($_POST['contrasenya']);
+				
+				$_SESSION=$_POST;
 
+				$sentencia="INSERT INTO usuario (Nombre,Contrasenya) VALUES
+					('$nombre','$contrasenya'";
+				//print $sentencia;
+				if($this->consulta($sentencia)){
+					//var_dump($_SESSION);
+					$this->desconectar();
+					$this->login();
+				}else{
+	 				print "<br>Se ha producido un error al registrarse en la base de datos<br>";
+	 	 			print "<br> El error es: " . mysqli_error($c) . "<br>";
+				}
+			
+			$this->desconectar();
+			}
 		}
+
 		function iniciar(){
-
+			if($this->conectar()){
+				$tabla="usuario";
+				$nombre=$_POST['nombre'];
+				$contrasenya=md5($_POST['contrasenya']);
+				$sentencia="SELECT * FROM $tabla WHERE Nombre='$nombre' AND Contrasenya='$contrasenya'";
+				//var_dump($sentencia);
+				if($this->consulta($sentencia)){
+					//echo "Sentencia correcta";
+					$resultado=$this->consulta($sentencia);
+					while($objeto=mysqli_fetch_object($resultado)){
+						//var_dump($objeto);
+						session_start();
+						$_SESSION["nombreusuario"]=$objeto->Nombre;
+						$_SESSION["contrasenya"]=md5($objeto->Contrasenya);
+						$_SESSION['estado'] = 'Logueado'; 
+						sleep(0);
+						//var_dump($_SESSION);
+				 	}
+				$this->desconectar();
+				}
+			}else{
+	 			echo "Error al conectar con la base de datos";
+			}
 		}
-		function valorar(){
 		
+		function valorar($usuario,$noticia,$pokeball){
+			if($this->conectar()){
+				$sentencia="INSERT INTO valoracion (Id_usuario, Id_noticia, valor) VALUES $usuario,$noticia,$pokeball";
+				if($this->consulta($sentencia)){
+					$resultado=$this->consulta($sentencia);
+					//var_dump($resultado);
+					$this->desconectar();
+				}else{
+	 				echo "Error con la sentencia";
+				}
+			}
 		}
 
 	}
