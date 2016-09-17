9fa3352a87b487fa02cf4764f0225a147bca3751
SrGio <sergionavarronavarro@msn.com>
2016-09-17 17:12:32 +0200

    Add files via upload

--- HGN/views/page.php
+++ HGN/views/page.php
@@ -8,15 +8,15 @@
 <body>
 
 <header>
-	<img id="logo" src="img/logo.png"></img>
+	<a href="index.php?action=principal"><img id="logo" src="img/logo.png"></img></a>
 	<a href="index.php?action=principal&Id_categoria=1"><img class="cat" src="img/actua.png"></img></a>
 	<a href="index.php?action=principal&Id_categoria=2"><img class="cat" src="img/sucesos.png"></img></a>
 	<a href="index.php?action=principal&Id_categoria=3"><img class="cat" src="img/trucos.png"></img></a>
-	<div id="pokedex"><a href="#"><img id="dex" src="img/pokedex1.png" onclick="this.src='img/Pokedex2.png';" onmouseout="this.src='img/Pokedex1.png';"/></img><span>Iniciar</span></a></div>
+	<a href="index.php?action=login"><img id="dex" src="img/pokedex1.png" onmouseover="this.src='img/dex2.png';" onmouseout="this.src='img/Pokedex1.png';"/></img></a>
 
 	<div id="linea1"></div>	
 </header>	
-<content>
+
 #SLIDER#
 	<div id="part2">
 
@@ -25,7 +25,9 @@
 
 
 	<footer>
-		<p>Hackathon GO NEWS | <a href="http://goo.gl/x4tE2M">Pokemon GO official web</a></p>
+		<p><a href="index.php?action=principal">Hackathon GO NEWS</a> | <a href="http://goo.gl/x4tE2M">Pokemon GO official web</a></p>
+		<p><a href="http://goo.gl/x4tE2M">Proyecto creado para Florida Universitaria</a></p>
+		<p><a href="index.php?action=principal">Ditto bailando</a> | <a href="http://es.pokemon.wikia.com/wiki/WikiDex">WikiDex</a></p>
 
 	</footer>
 
